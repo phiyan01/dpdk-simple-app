4287996353a1264c81c328c78738012d12e6c880
Phil Yang <phil.yang@arm.com>
2019-04-28 14:39:46 +0800

    Inital files

---
+++ setup.sh
@@ -0,0 +1,13 @@
+# SPDX-License-Identifier: BSD-3-Clause
+
+#!/bin/sh
+
+
+# DPDK Side
+# exprrt RTE_SDK=~/dpdk
+# export RTE_TARGET=arm64-armv8a-linux-gcc
+# make T=$RTE_TARGET O=$RTE_TARGET config
+# make T=$RTE_TARGET O=$RTE_TARGET
+
+export RTE_SDK=~/dpdk
+export RTE_TARGET=arm64-armv8a-linux-gcc
