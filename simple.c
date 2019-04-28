277d7fac7b0d9cc952d33f605e69732a225e96c3
Phil Yang <phil.yang@arm.com>
2019-04-28 14:58:13 +0800

    eal init

--- simple.c
+++ simple.c
@@ -2,9 +2,46 @@
  */
 
 #include <stdio.h>
+#include <stdlib.h>
+
+#include <rte_eal.h>
+#include <rte_common.h>
+#include <rte_log.h>
+
+/* Macros for printing using RTE_LOG */
+#define RTE_LOGTYPE_APP RTE_LOGTYPE_USER1
 
 int main(int argc, char *argv[])
 {
-	printf("\n");
+	int ret;
+
+	/*
+	 * EAL: Environment Abstract Layer"
+	 *
+	 * eal gets parameters from cli, returns number of parsed args
+	 *
+	 * cpu_init: fill cpu_info structure
+	 * log_init
+	 * config_init: create memory configuration in shared memory
+	 * pci_init: scan pci bus
+	 * memory_init (hugepages)
+	 * memzone_init: initialize memzone subsystem
+	 * alarm_init: for timer interrupts
+	 * timer_init
+	 * plugin init
+	 * dev_init: initialize and probe virtual devices
+	 * intr_init: create an interrupt handler thread
+	 * lcore_init: create a thread per lcore
+	 * pci_probe: probe all physical devices
+	 */
+	ret = rte_eal_init(argc, argv);
+	if (ret < 0)
+		rte_exit(EXIT_FAILURE, "EAL Init failed\n");
+
+	argc -= ret;
+	argv += ret;
+
+	/* There is no un-init for eal */
+
 	return 0;
 }
